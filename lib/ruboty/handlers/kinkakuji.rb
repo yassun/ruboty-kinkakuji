d27b8b1b82b80e0ce72ed7717f9b619eb3177a26
Taga Yasunori <yasukun.taga@gmail.com>
2015-01-10 13:06:52 +0900

    add kinkakuji images

--- lib/ruboty/handlers/kinkakuji.rb
+++ lib/ruboty/handlers/kinkakuji.rb
@@ -3,7 +3,14 @@ require "ruboty/kinkakuji/actions/kinkakuji"
 module Ruboty
   module Handlers
     class Kinkakuji < Base
+      KINKAKUJI_IMAGES = [
+      'http://www.shokoku-ji.jp/img/webcam/kinkaku/full/kinkaku01.jpg',
+      'http://www.shokoku-ji.jp/img/webcam/kinkaku/full/kinkaku02.jpg'
+      ]
       on /kinkakuji/, name: 'kinkakuji', description: 'Show kinkakuji realtime image'
+      def kinkakuji(message)
+        message.reply(KINKAKUJI_IMAGES.sample)
+      end
     end
   end
 end
